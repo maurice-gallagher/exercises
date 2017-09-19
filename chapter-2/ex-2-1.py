1f24585ba5fc010e20716f9de19b2da09daa9256
Cade Gallagher <maurice-gallagher@student.kirkwood.edu>
2017-09-19 13:18:35 +0000

    address messege

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,20 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Cade Gallagher")
 
 # display the address
-
+print("545 29th ave")
 
 # display the city, state and zip
-
+print("Amana, Iowa, 52203")
 
 # display the phone number
-
+print("319-540-9052")
 
 # display the course area
-
-
+print("CIS-121")
 
 
 
