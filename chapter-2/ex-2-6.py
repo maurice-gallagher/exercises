2075e661ef290872925285b2b8f3816b846deb27
Cade Gallagher <maurice-gallagher@student.kirkwood.edu>
2017-09-28 13:21:22 +0000

    -2-6

--- chapter-2/ex-2-6.py
+++ chapter-2/ex-2-6.py
@@ -8,28 +8,29 @@
 # Variables for purchase amount, state tax, county tax, total tax and total sale
 # all initialized as floats
 
-
 # Constants for the state and county tax rates
 
 
 # Get the amount of purchase from the user, casting it to a float.
 
-
 # Calculate the state sales tax.
-
 
 # Calculate the county sales tax.
 
-
 # Sum the total tax.
-
 
 # Calculate the total of the sale.
 
-
 # Print detailed information about the sale, formatting all values to two decimal places.
 
+amount = float(input('insert the cost of your item: '))
 
+STATE_TAX = 6.12/100
+COUNTY_TAX = 7/100
 
+total_tax = STATE_TAX + COUNTY_TAX
+total_price = total_tax + amount
+
+print('your total price is', format(total_price, ".2f"))
 
 
