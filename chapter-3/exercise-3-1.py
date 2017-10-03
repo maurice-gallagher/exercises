32768685aacbccf47952e332032a427c950fac6a
Cade Gallagher <maurice-gallagher@student.kirkwood.edu>
2017-10-03 13:26:56 +0000

    exercises 3-1 and 3-2

--- chapter-3/exercise-3-1.py
+++ chapter-3/exercise-3-1.py
@@ -12,21 +12,29 @@
 # Get the number for the day of the week.
 # be sure to format the input as an int
 
-
 # Determine the value to assign to the day of the week.
 # use a set of if ... elif ... etc. statements to test the day of the week value.
 
-
-
-
-
-
-
 # use the final else to display an error message if the number is out of range.
 
-
 # display the name of the day on the screen.
-
+DAY_OF_WEEK = int(input('please insert your number between 1-7 to see the day of the week: '))
+if DAY_OF_WEEK == 1:
+    print('Sunday')
+if DAY_OF_WEEK == 2:
+    print('Monday')
+if DAY_OF_WEEK == 3:
+    print('Tuesday')
+if DAY_OF_WEEK == 4:
+    print('Wednesday')
+if DAY_OF_WEEK == 5:
+    print('Thursday')
+if DAY_OF_WEEK == 6:
+    print('Friday')
+if DAY_OF_WEEK == 7:
+    print('Saturday')
+elif DAY_OF_WEEK > 7:
+    print('There are not more than 7 days in the week')
 
 
 
--- chapter-3/exercise-3-2.py
+++ chapter-3/exercise-3-2.py
@@ -9,24 +9,14 @@
 # you need length, width and area for A and for B
 # initialize these as floats
 
-
 # Get length A from the user and convert it to a float
-
-
 # Get width A from the user and convert it to a float
-
-
 # Get length B from the user and convert it to a float
-
-
 # Get width B from the user and convert it to a float
-
 
 # Calculate area A
 
-
 # Calculate area B
-
 
 # Print each area, formatting the values to 2 decimal places
 
@@ -36,3 +26,20 @@
 
 # else print "A and B are equal" message.
 
+Length_A = float(input('What is the length of rectangle A: '))
+Length_B = float(input('What is the length of rectangle B: '))
+Width_A = float(input('What is the width of rectangle A: '))
+Width_B = float(input('What is the width of rectangle B: '))
+
+Area_A = Length_A * Width_A
+Area_B = Length_B * Width_B
+
+print('The area of rectangle A is', format(Area_A, ".2f"))
+print('The area of rectangle B is', format(Area_B, ".2f"))
+
+if Area_A > Area_B:
+    print('Rectangle A is greater')
+elif Area_B > Area_A:
+    print('Rectangle B is greater')
+else:
+    print('Rectangle A and rectangle B are equal')
